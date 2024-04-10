7e6fd41449431da845821d54c8b53055a0c3b7b7
AlejandroRodriguez1998 <alex_cr31@hotmail.com>
2024-04-10 21:16:11 +0200

    Correcion url collections

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,11 +1,11 @@
 class CollectionsController < CollectionsBaseController
   protected
   def after_collection_create
-    redirect_to collections_url, notice: 'Collection was successfully created.' and return
+    redirect_to collections_path, notice: 'Collection was successfully created.' and return
   end
 
   def after_collection_update
-    redirect_to collections_url, notice: "Collection was successfully updated." and return
+    redirect_to collections_path, notice: "Collection was successfully updated." and return
   end
 
   def after_collection_delete
